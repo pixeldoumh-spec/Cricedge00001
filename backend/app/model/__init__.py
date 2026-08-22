485c300ff3898e7ac06bf54236bead44328f7e09
pixeldoumh-spec <pixeldoumh@gmail.com>
2026-08-22 17:10:53 +0530

    feat(backend): add new model package

---
+++ backend/app/model/__init__.py
@@ -0,0 +1,8 @@
+"""CricEdge model package.
+
+The production cricket prediction model will be rebuilt here from scratch.
+"""
+
+from app.model.predictor import PredictionEngine
+
+__all__ = ["PredictionEngine"]
